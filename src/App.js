313a530360058c396f82b14f85c42473a0ef2aee
youngsik-hwang <youngisman@gmail.com>
2024-01-01 12:47:57 +0900

    학습내용 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React,{Component} from "react";
-import ScrollBox from "./ScrollBox";
+import LifeCycleSample from "./LifeCycleSample";
+import ErrorBoundary from "./ErrorBoundary";
+
+function getRandomColor(){
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component{
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () =>{
+    this.setState({
+      color: getRandomColor()
+    })
+  }
   render(){
     return(
       <div>
-        <ScrollBox ref={(ref) => this.scrollBox=ref} />
-        <button onClick={()=> this.scrollBox.scrollToBottom()}>맨 밑으로</button>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color}/>  
+        </ErrorBoundary>        
       </div>
     );
   }
